d0165b705992836b28ac549a4bfaace4cf15bf08
leeobby <leeobbyw@gmail.com>
2024-12-14 12:23:37 +0700

    chore: init packages dep

---
+++ src/services/HomeServices.ts
@@ -0,0 +1,5 @@
+export const getPlaceHolder = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  const data = await response.json();
+  return data;
+};
