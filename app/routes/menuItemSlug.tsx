ad1cc89ab893e1c89bc2934108d1a6a662ca64fc
leeobby <leeobbyw@gmail.com>
2025-01-21 21:24:27 +0700

    feat: add review action at menu item

--- app/routes/menuItemSlug.tsx
+++ app/routes/menuItemSlug.tsx
@@ -2,16 +2,22 @@ import type { paths } from '@/schema';
 import type { Route } from './+types/menuItemSlug';
 
 import { ENV } from '@/env';
-import { Debug } from '@/components/ui/debug';
 import { StarFilledIcon, StarIcon } from '@radix-ui/react-icons';
-import { MessageSquareQuoteIcon, PinIcon } from 'lucide-react';
-import { Link, useOutletContext } from 'react-router';
+import { MessageSquareQuoteIcon } from 'lucide-react';
+import { Link, useOutletContext, Form } from 'react-router';
 import { Avatar, AvatarFallback, AvatarImage } from '@radix-ui/react-avatar';
 import SearchForm from '@/components/shared/SearchForm';
-import { auth } from '@/lib/auth';
+import { accessToken, auth } from '@/lib/auth';
 import { Textarea } from '@/components/ui/textarea';
 import { Button } from '@/components/ui/button';
 import { useState } from 'react';
+import { Label } from '@/components/ui/label';
+import { parseWithZod } from '@conform-to/zod';
+import { useForm } from '@conform-to/react';
+import { z } from 'zod';
+
+type MenuItemReviewResponse =
+  paths['/menu-items/{slug}/reviews']['patch']['responses'][200]['content']['application/json'];
 
 type MenuItemSlugResponse =
   paths['/menu-items/{slug}']['get']['responses'][200]['content']['application/json'];
@@ -32,9 +38,59 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Route({ loaderData }: Route.ComponentProps) {
+const ReviewSchema = z.object({
+  rating: z.number({ message: 'This field is required' }),
+  comment: z.string({ message: 'This field is required' }),
+});
+
+export async function clientAction({
+  request,
+  params,
+}: Route.ClientActionArgs) {
+  const token = await accessToken.get();
+  let formData = await request.formData();
+  const submission = parseWithZod(formData, {
+    schema: ReviewSchema,
+  });
+
+  if (submission.status !== 'success') return submission.reply();
+
+  const response = await fetch(
+    `${ENV.VITE_BACKEND_API_URL}/menu-items/${params.menuItemSlug}/reviews`,
+    {
+      method: 'POST',
+      body: JSON.stringify(submission.value),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + token,
+      },
+    }
+  );
+  const menuItem: MenuItemReviewResponse = await response.json();
+
+  if (!menuItem) {
+    console.error(response);
+  }
+  return;
+}
+
+export default function Route({
+  loaderData,
+  actionData,
+}: Route.ComponentProps) {
   const { menuItem } = loaderData;
   const searchFormStatus: boolean = useOutletContext();
+  const TOTAL_STARS: number = 5;
+  const [rating, setRating] = useState<number>(0);
+  const [hover, setHover] = useState<number>(0);
+
+  const [form, fields] = useForm({
+    shouldValidate: 'onBlur',
+    lastResult: actionData,
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema: ReviewSchema });
+    },
+  });
 
   return (
     <div className="flex flex-col gap-4 p-5 mb-20">
@@ -57,29 +113,51 @@ export default function Route({ loaderData }: Route.ComponentProps) {
       <p className="dark:text-white">{menuItem.description}</p>
       <div>
         {auth?.isAuthenticated && (
-          <div className="bg-slate-200 dark:bg-slate-700 pl-4 pr-4 pt-5 pb-5 rounded-lg">
+          <Form
+            className="bg-slate-200 dark:bg-slate-700 pl-4 pr-4 pt-5 pb-5 rounded-lg"
+            method="patch"
+            id={form.id}
+            onSubmit={form.onSubmit}
+          >
             <h3 className="font-bold text-cyan-600 dark:text-cyan-300 underline mb-1">
               What do you think?
             </h3>
             <div className="flex items-center gap-1 mb-3 dark:text-slate-300">
               Rating :
-              <StarIcon />
-              <StarIcon />
-              <StarIcon />
-              <StarIcon />
-              <StarIcon />
+              {[...Array(TOTAL_STARS)].map((_, index) => {
+                const currentRating = index + 1;
+                return (
+                  <Label key={index}>
+                    <input
+                      type="radio"
+                      name={fields.rating.name}
+                      className="hidden"
+                      value={currentRating}
+                      onChange={() => setRating(currentRating)}
+                    />
+                    <StarIcon
+                      color={
+                        currentRating <= (hover || rating) ? 'orange' : 'gray'
+                      }
+                      onMouseEnter={() => setHover(currentRating)}
+                      onMouseLeave={() => setHover(0)}
+                    />
+                  </Label>
+                );
+              })}
             </div>
             <Textarea
+              name={fields.comment.name}
               placeholder="What do you think about this food ?"
               className="mb-2 dark:text-slate-200"
             />
             <Button
+              type="submit"
               className="max-w-40 self-center mt-3 dark:bg-yellow-500 dark:hover:bg-yellow-400"
-              disabled
             >
               Write Review
             </Button>
-          </div>
+          </Form>
         )}
 
         {!auth?.isAuthenticated && (
