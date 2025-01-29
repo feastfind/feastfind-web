a1e521b7523e2b8ab20ed0d07213ecb0583cd462
leeobby <leeobbyw@gmail.com>
2025-01-29 22:28:19 +0700

    feast: map interactive for explore page

--- app/routes/explore.tsx
+++ app/routes/explore.tsx
@@ -1,11 +1,29 @@
-import maplibregl from 'maplibre-gl';
-import 'maplibre-gl/dist/maplibre-gl.css';
-import { useEffect, useState } from 'react';
-import SearchForm from '@/components/shared/SearchForm';
 import type { Route } from './+types/explore';
-import { useOutletContext } from 'react-router';
+import type { paths } from '@/schema';
+import type { itemPlaceSchema } from '@/schema/schema';
 
-const { LngLat } = maplibregl;
+import { useEffect, useState } from 'react';
+import { ENV } from '@/env';
+import Map, { Marker, NavigationControl } from 'react-map-gl';
+import { Banknote, MapPin } from 'lucide-react';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+} from '@/components/ui/drawer';
+import { Button } from '@/components/ui/button';
+import { Label } from '@radix-ui/react-label';
+import { StarFilledIcon } from '@radix-ui/react-icons';
+import { formatRupiah } from '@/lib/utils';
+import { Link } from 'react-router';
+
+type PlacesResponse =
+  paths['/places']['get']['responses'][200]['content']['application/json'];
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -14,36 +32,22 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-const style: maplibregl.StyleSpecification = {
-  version: 8,
-  sources: {
-    osm: {
-      type: 'raster',
-      tiles: ['https://tile.openstreetmap.org/{z}/{x}/{y}.png'],
-      tileSize: 256,
-      attribution: '&copy; OpenStreetMap Contributors',
-      maxzoom: 19,
-    },
-  },
-  layers: [
-    {
-      id: 'osm',
-      type: 'raster',
-      source: 'osm',
-    },
-  ],
-};
+export async function clientLoader() {
+  const response = await fetch(`${ENV.VITE_BACKEND_API_URL}/places`);
+  const placesJSON: PlacesResponse = await response.json();
 
-const places = [
-  {
-    name: 'Manabu',
-    latitude: 0.9261549301037288,
-    longitude: 104.43826874232784,
-    description: 'Lokasi manabu.',
-  },
-];
+  return { placesJSON: placesJSON };
+}
 
-export default function Route() {
+export default function Route({ loaderData }: Route.ComponentProps) {
+  const { placesJSON } = loaderData;
+  const [openDrawer, setOpenDrawer] = useState<boolean>(false);
+  const [currentPlace, setCurrentPlace] = useState<itemPlaceSchema | null>(
+    null
+  );
+  const [accessToken, setAccessToken] = useState<string>(
+    ENV.VITE_MAPBOX_ACCESS_TOKEN
+  );
   const [userLocation, setUserLocation] = useState<{
     latitude: number;
     longitude: number;
@@ -54,69 +58,121 @@ export default function Route() {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           const { latitude, longitude } = position.coords;
+
           setUserLocation({ latitude, longitude });
         },
+
         (error) => {
-          console.error('Error fetching location:', error);
+          console.error('Error get user location: ', error);
         }
       );
     } else {
-      console.error('Geolocation is not supported by this browser.');
+      console.log('Geolocation is not supported by this browser');
+
+      setUserLocation({
+        latitude: 0.9261549301037288,
+        longitude: 104.43826874232784,
+      });
     }
+    return () => {
+      setUserLocation(null);
+    };
   }, []);
 
-  useEffect(() => {
-    if (userLocation) {
-      const { latitude, longitude } = userLocation;
-
-      const map = new maplibregl.Map({
-        container: 'map',
-        style: style,
-        center: [longitude, latitude],
-        zoom: 17,
-      });
-
-      new maplibregl.Marker().setLngLat([longitude, latitude]).addTo(map);
-
-      new maplibregl.Popup({ offset: 25 })
-        .setLngLat([longitude, latitude])
-        .setHTML('<h3>Current location.</p>')
-        .addTo(map);
-
-      places.map((place) => {
-        const { latitude, longitude } = place;
-
-        const placeDistance = new LngLat(longitude, latitude);
-        const userDistance = new LngLat(
-          userLocation.longitude,
-          userLocation.latitude
-        );
-
-        const distance = userDistance.distanceTo(placeDistance);
-
-        new maplibregl.Marker().setLngLat([longitude, latitude]).addTo(map);
-
-        new maplibregl.Popup({ offset: 25 })
-          .setLngLat([longitude, latitude])
-          .setHTML(
-            `<p>place: ${place.name}<br>distance: ${distance.toFixed(2)}</p>`
-          )
-          .addTo(map);
-      });
-
-      map.addControl(new maplibregl.NavigationControl());
-
-      return () => map.remove();
-    }
-  }, [userLocation]);
-
-  const searchFormStatus: boolean = useOutletContext();
-
   return (
-    <div className="flex flex-col gap-4 p-5 mt-16">
-      <div className="text-2xl font-medium dark:text-white">Explore Places</div>
-      {searchFormStatus && <SearchForm />}
-      <div id="map" style={{ width: '100%', height: '500px' }}></div>
+    <div className="relative overflow-hidden max-w-[500px] h-[calc(100vh-8rem)]">
+      <div className="fixed top-16 bottom-16 left-0 right-0 max-w-[500px] mx-auto">
+        {userLocation && (
+          <Map
+            mapboxAccessToken={accessToken}
+            initialViewState={{
+              longitude: userLocation?.longitude,
+              latitude: userLocation?.latitude,
+              zoom: 14,
+            }}
+            style={{ maxWidth: '100%', height: '100%' }}
+            mapStyle="mapbox://styles/mapbox/streets-v9"
+          >
+            <Marker
+              longitude={userLocation?.longitude}
+              latitude={userLocation?.latitude}
+              color="red"
+            />
+            {placesJSON.map((place) => (
+              <Marker
+                key={place.id}
+                longitude={place?.longitude}
+                latitude={place?.latitude}
+                onClick={() => {
+                  setOpenDrawer(true);
+                  setCurrentPlace({ place });
+                }}
+                anchor="center"
+              >
+                <div className="flex flex-col items-center">
+                  <MapPin className="size-8 text-red-500" />
+                  <span className="font-medium ">{place.name}</span>
+                </div>
+              </Marker>
+            ))}
+            <NavigationControl />
+          </Map>
+        )}
+      </div>
+
+      <Drawer open={openDrawer} onOpenChange={setOpenDrawer}>
+        <DrawerContent className="max-w-[500px] mx-auto">
+          <DrawerHeader className="hidden">
+            <DrawerTitle></DrawerTitle>
+            <DrawerDescription></DrawerDescription>
+          </DrawerHeader>
+          <div className="mx-auto w-full max-w-sm">
+            <div className="p-4 pb-0 flex">
+              {currentPlace?.place && (
+                <>
+                  <div className="w-32 h-full bg-gray-50">
+                    <img
+                      alt="menu item"
+                      src={`${currentPlace?.place?.images[0]}-/resize/300/`}
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+
+                  <div className="flex-1 p-4">
+                    <Label className="text-md text-amber-800 dark:text-yellow-500 cursor-pointer font-bold">
+                      {currentPlace?.place.name}
+                    </Label>
+                    <div className="flex items-center gap-1 dark:text-slate-300">
+                      {currentPlace?.place.ratingScore}
+                      <StarFilledIcon className="text-amber-600 dark:text-yellow-500" />
+                    </div>
+                    <div className="flex items-center gap-2 font-light text-xs text-emerald-800 dark:text-cyan-300">
+                      <Banknote />
+                      <Label>{`${formatRupiah(
+                        parseInt(String(currentPlace?.place?.priceMin))
+                      )}-${formatRupiah(
+                        parseInt(String(currentPlace?.place?.priceMax))
+                      )}`}</Label>
+                    </div>
+                  </div>
+                </>
+              )}
+            </div>
+            <DrawerFooter>
+              {currentPlace?.place && (
+                <Button className="bg-red-900" asChild>
+                  <Link to={`/${currentPlace?.place?.slug}`} viewTransition>
+                    See details
+                  </Link>
+                </Button>
+              )}
+              <DrawerClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </div>
+        </DrawerContent>
+      </Drawer>
     </div>
   );
 }
